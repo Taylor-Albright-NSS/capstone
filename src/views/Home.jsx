997aa2f0d2a83b540323eb8c3e054c0292098a2d
Taylor Albright <jtayloralbright@gmail.com>
2024-10-17 17:30:58 -0500

    Added background, banner, and border on the welcome page.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,12 +4,11 @@ export const Home = () => {
     return (
         <div className='home-container'>
             <div className='welcome'>
-            <img alt='no logo yet' />
+            <img src='/assets/icons/Galvadia Banner.png' className='welcome-banner'/>
                 <h1>Welcome To The The Galvadian Armory!</h1>
                 <p>The armory is an interactive website that allows you to create a character sheet for your in-game Galvadia
                 characters.</p>
             </div>
-
         </div>
     )
 }
