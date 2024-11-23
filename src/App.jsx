80c01dd51488d45446023b0aa0a3f0f9ec82f65a
Taylor Albright <jtayloralbright@gmail.com>
2024-11-23 14:29:23 -0600

    deleting 5 things

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/auth/Login'
 import { Register } from './components/auth/Register'
 import { Authorized } from './views/Authorized'
 import { ApplicationViews } from './views/ApplicationViews'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
